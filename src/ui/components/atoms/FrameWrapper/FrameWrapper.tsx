9c79cd165ec829aa776c8798e1d385ccfb701a49
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-12-24 05:08:47 +0200

    fixed style

--- src/ui/components/atoms/FrameWrapper/FrameWrapper.tsx
+++ src/ui/components/atoms/FrameWrapper/FrameWrapper.tsx
@@ -14,7 +14,7 @@ export const FrameWrapper = ({ children }: FrameWrapperProps) => {
                 "relative flex flex-col justify-end items-start",
                 "w-[54.5rem] h-[33.7rem] pb-[5.8rem] px-[11rem] gap-y-[2rem]",
                 "max-tablet:w-[32.8rem] max-tablet:h-[17rem] max-tablet:pl-[7rem] max-tablet:pr-[3rem] max-tablet:pb-[2rem] max-tablet:gap-y-[1rem]",
-                "max-mobile:w-[25rem] max-mobile:h-[16rem] max-mobile:px-[3rem] max-mobile:pb-[2.4rem]"
+                "max-mobile:w-[25rem] max-mobile:h-[16rem] max-mobile:pl-[4rem] max-mobile:pr-[3rem] max-mobile:pb-[2.4rem]"
             )}
         >
             {children}
