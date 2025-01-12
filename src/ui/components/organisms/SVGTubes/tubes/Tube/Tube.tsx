d9e6e7e90ecd628ee663d7f65eb35963a62c26e2
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2025-01-12 18:30:42 +0200

    test

--- src/ui/components/organisms/SVGTubes/tubes/Tube/Tube.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { TubeProps } from "@/src/types";
-
-export const Tube = ({ x, y, width, tubeHeight, className }: TubeProps) => {
-    return (
-        <rect x={x} y={y} width={width} height={tubeHeight} className={className} />
-    )
-};
